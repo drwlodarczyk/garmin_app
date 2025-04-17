83dade02957ae7bdb9cf56ff4e43c7245a4b6ba8
drwlodarczyk <dawid.r.wlodarczyk@gmail.com>
2025-04-17 17:06:29 +0200

    footer and header

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Ustawienie EJS i ścieżek
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
